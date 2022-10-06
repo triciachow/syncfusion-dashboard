1949419d448355a8de91fdaa2e9ce3f77771817b
triciachow0212 <suyeechow0212@gmail.com>
2022-10-06 17:58:25 +0800

    Complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,6 +36,7 @@ const Navbar = () => {
     handleClick,
     screenSize,
     setScreenSize,
+    currentColor,
   } = useStateContext();
 
   useEffect(() => {
@@ -61,28 +62,28 @@ const Navbar = () => {
         <NavButton
           title="Menu"
           customFunc={() => setActiveMenu(!activeMenu)}
-          color="blue"
+          color={currentColor}
           icon={<AiOutlineMenu />}
         />
         <div className="flex">
           <NavButton
             title="Cart"
             customFunc={() => handleClick("cart")}
-            color="blue"
+            color={currentColor}
             icon={<FiShoppingCart />}
           />
           <NavButton
             title="Chat"
             dotColor="#03C9D7"
             customFunc={() => handleClick("chat")}
-            color="blue"
+            color={currentColor}
             icon={<BsChatLeft />}
           />
           <NavButton
             title="Notification"
             dotColor="rgb(254, 201, 15)"
             customFunc={() => handleClick("notification")}
-            color="blue"
+            color={currentColor}
             icon={<RiNotification3Line />}
           />
           <TooltipComponent content="Profile" position="BottomCenter">
